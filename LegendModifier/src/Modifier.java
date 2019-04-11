0595d632bc30855ca0ddf27a0611b3c6133f27ac
duxiaopan <>
2019-04-11 17:27:34 +0800

    丰富代码功能

--- LegendModifier/src/Modifier.java
+++ LegendModifier/src/Modifier.java
@@ -1,39 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
-
-import bean.GoodsInfo;
 import file.FileUtil;
 
 public class Modifier {
 	private static String root = "D:/Documents/Tencent Files/364499854/FileRecv/MonItems/MonItems/";
+	private static String goodsFile = "D:/Documents/Tencent Files/364499854/FileRecv/物品名称.txt";
+	private static String mapDataFile = "D:/Documents/Tencent Files/364499854/FileRecv/地图数据.txt";
 	
 	public static void main(String[] args) {
-		List<GoodsInfo> list = showGoodsFromWhere(root,"屠龙");
-		for(GoodsInfo info :list){
-			System.out.println(info.toString());
+//		FileUtil.errorRecovery(goodsFile,root);
+		String content = FileUtil.getTxtFileContenet("D:/Documents/Tencent Files/364499854/FileRecv/", "地图数据.txt");
+		
+		String[] monsters = FileUtil.getDirectoryFileList(root);
+		for(String monster:monsters){
+			monster = formartMonsterName(monster);
+			if(content.contains(monster)){
+//				System.out.println("地图中有"+monster);
+			}else{
+				System.out.println("地图中没有"+monster);
+			}
+			
 		}
     }
 	
-	public static List<GoodsInfo> showGoodsFromWhere(String fileDir,String goods){
-		List<GoodsInfo> list = new ArrayList<>();
-		String[] files = FileUtil.getDirectoryFileList(fileDir);
-		for(String fileName: files){
-			String content = FileUtil.readGoodsInfo(fileDir+fileName,goods).trim();
-			if(!"无".equals(content)){
-				content = content.replace('\t', ' ');
-				String gailv = content.substring(0,content.indexOf(" "));
-                String otherInfo = content.substring(content.indexOf(" "),content.length()).trim();
-                String[] others = otherInfo.split(" ");
-                System.out.println(content);
-				GoodsInfo goodsInfo = new GoodsInfo();
-				goodsInfo.setGailv(gailv);
-				goodsInfo.setName(others[0]);
-				goodsInfo.setMonster(others[1]);
-				list.add(goodsInfo);
-			}
+	private static String formartMonsterName(String name){
+		name = name.replace(".txt", "");
+		for(int i=0;i<10;i++){
+			name = name.replace(String.valueOf(i), "");
 		}
-		return list;
+		return name;
 	}
+	
+	
 	
 	
 	
